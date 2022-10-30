e8652f1c7b4f682fbdb6e37f971b57de3d7dd31c
ongboonhan <edvinongbh@gmail.com>
2022-10-30 09:10:05 +0800

    Completed

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,3 +1,48 @@
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+export default function AddScreen({ route, navigation }) {
+  const [text, setText] = useState("");
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={styles.label}>Add your todo</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={(newText) => setText(newText)}
+      ></TextInput>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Notes", { text })}
+          style={[styles.button, styles.submitButton]}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={[styles.button, styles.cancelButton]}
+        >
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ marginTop: 40, color: "grey" }}>
+        This is what you typed:
+      </Text>
+      <Text style={{ color: "#333", marginTop: 10 }}>{text}</Text>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   label: {
     fontWeight: "bold",
--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -7,27 +7,21 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
 
-export default function NotesScreen({ navigation }) {
-  const [notes, setNotes] = useState([
-    { title: "Dinner", done: false, id: "0" },
-    { title: "sleep", done: false, id: "1" },
-  ]);
+export default function NotesScreen({ navigation, route }) {
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <TouchableOpacity onPress={addNote}>
-          <Entypo
-            name="new-message"
-            size={24}
-            color="black"
-            style={{ marginRight: 20 }}
-          />
-        </TouchableOpacity>
-      ),
-    });
-  });
+    if (route.params?.text) {
+      const newNote = {
+        title: route.params.text,
+        done: false,
+        id: notes.length.toString(),
+      };
+      setNotes([...notes, newNote]);
+    }
+  }, [route.params?.text]);
 
   function addNote() {
     navigation.navigate("Add Note");
